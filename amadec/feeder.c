1dbffaec74884a3026e82d01c124b9def4b1ece8
jiejing.wang <jiejing.wang@amlogic.com>
2018-11-15 15:34:03 +0800

    audio: Libaudio: add license header [3/3]
    PD#SWPL-2111
    
    Problem:
    no license source files
    
    Solution:
    add license header
    
    Verify:
    verify by locally
    
    Change-Id: Ib9cdd4d33c544347ab422b99c5f6cf3fa8f1548d
    Signed-off-by: jiejing.wang <jiejing.wang@amlogic.com>

--- amadec/feeder.c
+++ amadec/feeder.c
@@ -1,13 +1,23 @@
-/**
- * \file feeder.c
- * \brief  Functions of Feeder
- * \version 1.0.0
- * \date 2011-03-08
- */
-/* Copyright (C) 2007-2011, Amlogic Inc.
- * All right reserved
+/*
+ * Copyright (C) 2018 Amlogic Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *  DESCRIPTION:
+ *      brief  Functions of Feeder.
  *
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
