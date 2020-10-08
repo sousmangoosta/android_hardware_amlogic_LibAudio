a8f4deeb0ae66f81c95e6af7ec4c98455759dec4
Ronald Claveau <sousmangoosta@aliel.fr>
2020-10-08 15:56:25 +0200

    Remove libmediaextractor dependency

--- amadec/omx_audio/Android.mk
+++ amadec/omx_audio/Android.mk
@@ -28,8 +28,7 @@ endif
 
 LOCAL_SHARED_LIBRARIES += libutils libmedia libz libbinder libdl libcutils libc libstagefright \
                           libstagefright_omx libmedia_native liblog \
-                          libstagefright_foundation \
-                          libmediaextractor
+                          libstagefright_foundation
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_TAGS := optional
